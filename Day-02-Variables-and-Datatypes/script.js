0b5717c8529dc05288c29bd76e9bd7639f9a1410
saikatmaji <saikatmaji6295@gmail.com>
2026-06-06 21:50:44 +0530

    (feat) Day 02 Variables and Datatypes

---
+++ Day-02-Variables-and-Datatypes/script.js
@@ -0,0 +1,16 @@
+// Variables
+
+// variable declaration with const keyword
+const account = 1234;
+
+// account = 5678; // const => re-declare ❌ | re-assign ❌
+
+console.log(account);
+
+
+// const => Global Scope ✅, Function Scope ✅, Block Scope ✅
+if(true){
+    // const a = 20;
+}
+
+// console.log(a); // ReferenceError
