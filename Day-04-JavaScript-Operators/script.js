625c6fd9ea3f19182b0044162d2d73b0b66dc273
saikatmaji <saikatmaji6295@gmail.com>
2026-07-03 23:58:21 +0530

    (feat) Day 04 JavaScript Operators

--- Day-04-JavaScript-Operators/script.js
+++ Day-04-JavaScript-Operators/script.js
@@ -41,3 +41,80 @@
 
 // Returns the remainder after division.
 // 12 % 5 = 2
+
+
+
+
+// ******************************************************************************************************
+
+
+
+
+// =========================================
+// Comparison Operators
+// =========================================
+
+// Comparison operators are used to compare two values.
+// They always return either true or false.
+
+// Operators:
+// ==   Equal to (Loose Equality)
+// ===  Strict Equal to
+// !=   Not Equal to (Loose Inequality)
+// !==  Strict Not Equal to
+// >    Greater Than
+// <    Less Than
+// >=   Greater Than or Equal to
+// <=   Less Than or Equal to
+
+
+// Equal to (==)
+
+// Performs type conversion before comparison.
+
+// 12 == 13      // false
+// 12 == "12"    // true
+
+
+// Strict Equal to (===)
+
+// Compares both value and data type.
+
+// 12 === "12"   // false
+// 12 === 12     // true
+
+
+// Not Equal to (!=)
+
+// Performs type conversion before comparison.
+
+// 12 != 13          // true
+// "harsh" != "ankur" // true
+
+
+// Strict Not Equal to (!==)
+
+// Compares both value and data type.
+
+// 12 !== "12"   // true
+// 12 !== 12     // false
+
+
+// Greater Than (>)
+
+// 12 > 113      // false
+
+
+// Less Than (<)
+
+// 12 < 113      // true
+
+
+// Greater Than or Equal To (>=)
+
+// 22 >= 22      // true
+
+
+// Less Than or Equal To (<=)
+
+// 32 <= 32      // true
