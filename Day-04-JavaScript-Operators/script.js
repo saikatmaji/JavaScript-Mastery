2880cab9ddb03178053c4272e236722f9e1abff7
saikatmaji <saikatmaji6295@gmail.com>
2026-07-03 23:56:24 +0530

    (feat) Day 04 JavaScript Operators

---
+++ Day-04-JavaScript-Operators/script.js
@@ -0,0 +1,43 @@
+// =========================================
+// Arithmetic Operators
+// =========================================
+
+// Arithmetic operators are used to perform mathematical calculations.
+
+// Operators:
+// +  Addition
+// -  Subtraction
+// *  Multiplication
+// /  Division
+// %  Modulus (Remainder)
+// ** Exponentiation (Power)
+
+
+// Addition (+)
+
+// Number Addition
+// 1 + 2 = 3
+
+// String Concatenation
+// "ha" + "rsh" = "harsh"
+
+
+// Subtraction (-)
+
+// 12 - 2 = 10
+
+
+// Multiplication (*)
+
+// 12 * 2 = 24
+
+
+// Division (/)
+
+// 12 / 2 = 6
+
+
+// Modulus (%)
+
+// Returns the remainder after division.
+// 12 % 5 = 2
