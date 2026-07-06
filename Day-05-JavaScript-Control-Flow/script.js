400f2cbc2b83d4ddf9ad6891ca67fc5516ff91ad
saikatmaji <saikatmaji6295@gmail.com>
2026-07-06 23:42:00 +0530

    (feat) Day 05 JavaScript Control Flow

---
+++ Day-05-JavaScript-Control-Flow/script.js
@@ -0,0 +1,14 @@
+// =========================================
+// If...Else Statement
+// =========================================
+
+// The if...else statement is used to execute different blocks of code based on whether a condition is true or false.
+
+// Example
+let a = 12;
+
+if (a > 15) {
+  console.log("TRUE");
+} else {
+  console.log("FALSE");
+}
