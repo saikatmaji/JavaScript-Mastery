d0ed653be5a71d7747b0040d9e036efd953a873e
saikatmaji <saikatmaji6295@gmail.com>
2026-07-10 23:16:44 +0530

    (feat) Day 06 JavaScript Loops

---
+++ Day-06-JavaScript-Loops/script.js
@@ -0,0 +1,21 @@
+// =========================================
+// For Loop
+// =========================================
+
+// Loops are used to repeat a block of code.
+
+
+// The for loop is used when the number of iterations is known before the loop starts.
+
+// Syntax:
+// for (initialization; condition; increment/decrement) {
+//   // code to execute
+// }
+
+
+// Example:
+// Print numbers from 1 to 100
+
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
