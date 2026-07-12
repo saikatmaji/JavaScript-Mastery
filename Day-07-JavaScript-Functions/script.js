ef66f95d8d45ec661f9881070b0cf20caae7c5b5
saikatmaji <saikatmaji6295@gmail.com>
2026-07-12 23:40:57 +0530

    (feat) Day 07 JavaScript Functions

---
+++ Day-07-JavaScript-Functions/script.js
@@ -0,0 +1,20 @@
+// =========================================
+// Function Use Case
+// =========================================
+
+// Functions allow us to group reusable code. The code inside a function does not execute until the function is called.
+
+
+// Function Declaration
+function dance() {
+  console.log("dance");
+  console.log("dance");
+  console.log("dance");
+  console.log("dance");
+  console.log("dance");
+  console.log("dance");
+}
+
+
+// Function Call
+dance();
