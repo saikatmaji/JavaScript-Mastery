ce27548ef9f7acfdeebb04a6443c1683d2f50cd1
saikatmaji <saikatmaji6295@gmail.com>
2026-07-16 23:34:55 +0530

    (feat) Day 08 JavaScript Arrays

---
+++ Day-08-JavaScript-Arrays/script.js
@@ -0,0 +1,11 @@
+// =========================================
+// Array Use Case
+// =========================================
+
+// Arrays are used to store multiple values in a single variable.
+
+
+// Example-
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+console.log(fruits);
