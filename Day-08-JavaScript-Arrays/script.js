3a6e54ed671830b2a37b8a7e055b0d01a687b9dd
saikatmaji <saikatmaji6295@gmail.com>
2026-07-16 23:38:38 +0530

    (feat) Day 08 JavaScript Arrays

--- Day-08-JavaScript-Arrays/script.js
+++ Day-08-JavaScript-Arrays/script.js
@@ -9,3 +9,40 @@
 let fruits = ["Apple", "Banana", "Mango", "Orange"];
 
 console.log(fruits);
+
+
+
+
+// ******************************************************************************************************
+
+
+
+
+// =========================================
+// Creating and Accessing Arrays
+// =========================================
+
+// Arrays are used to store multiple values in a single variable.
+
+
+
+// Creating an Array
+let marks = [10, 40, 52, 37, 92];
+
+
+
+// Accessing Array Elements
+
+// Array indexing starts from 0.
+
+console.log(marks[0]); // 10
+console.log(marks[1]); // 40
+console.log(marks[2]); // 52
+console.log(marks[3]); // 37
+console.log(marks[4]); // 92
+
+
+
+// Accessing an index that does not exist returns undefined.
+
+console.log(marks[10]); // undefined
