a942bc5fc138a50202abcb67497f60ed533eb89b
saikatmaji <saikatmaji6295@gmail.com>
2026-07-21 21:26:05 +0530

    (feat) Day 09 JavaScript Objects

---
+++ Day-09-JavaScript-Objects/script.js
@@ -0,0 +1,25 @@
+// =========================================
+// Arrays vs Objects
+// =========================================
+
+// Arrays are used to store multiple values of the same type or related data.
+
+// Objects are used to store multiple properties of a single entity.
+
+
+
+// Example-
+
+// Array
+let students = ["Rahul", "Aman", "Priya"];
+
+
+// Object
+let student = {
+  name: "Rahul",
+  age: 21,
+  city: "Kolkata",
+};
+
+console.log(students);
+console.log(student);
