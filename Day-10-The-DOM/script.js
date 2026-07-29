c3f5655ee7355e42d646f0fa29d0c435e1893f79
saikatmaji <saikatmaji6295@gmail.com>
2026-07-29 23:19:05 +0530

    (feat) Day 10 The DOM

---
+++ Day-10-The-DOM/script.js
@@ -0,0 +1,31 @@
+// =========================================
+// What is the DOM?
+// =========================================
+
+// DOM (Document Object Model) is a programming interface that represents an HTML document as a tree of objects.
+
+// Everything you see on a webpage, such as headings, paragraphs, images, buttons, links, and forms is part of the DOM.
+
+// The DOM allows JavaScript to:
+// - Access HTML elements
+// - Modify content
+// - Change styles
+// - Handle events
+// - Create or remove elements
+
+
+
+// DOM Tree Structure
+
+// The DOM represents an HTML document as a tree structure.
+
+// Main types of DOM nodes:
+// - Document
+// - Element
+// - Text
+// - Comment
+
+
+
+
+// ******************************************************************************************************
