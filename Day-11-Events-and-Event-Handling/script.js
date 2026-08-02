aa85ca0ca118f91da08faa8cbc3426dc9645ea0f
saikatmaji <saikatmaji6295@gmail.com>
2026-08-02 23:50:53 +0530

    (feat) Day 11 Events and Event Handling

---
+++ Day-11-Events-and-Event-Handling/script.js
@@ -0,0 +1,18 @@
+// =========================================
+// What is an Event?
+// =========================================
+
+// An event is any action or occurrence that happens in the browser.
+
+// Examples of events include:
+// - Clicking a button
+// - Typing in an input field
+// - Moving the mouse
+// - Pressing a keyboard key
+// - Submitting a form
+// - Scrolling the page
+
+
+
+
+// ******************************************************************************************************
