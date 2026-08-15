e3f47fec20104bfbe1eb97ce4fd931b1c8868f35
saikatmaji <saikatmaji6295@gmail.com>
2026-08-15 23:04:14 +0530

    (feat) Day 13 Timers and Intervals

---
+++ Day-13-Timers-and-Intervals/2_setInterval.js
@@ -0,0 +1,19 @@
+// setInterval()
+
+// setInterval() is used to execute a function repeatedly after a specified amount of time.
+
+// Unlike setTimeout(), setInterval() keeps running the function repeatedly at the given time interval.
+
+
+// Syntax
+
+// setInterval(callbackFunction, delay);
+
+
+// Example
+
+// Print "Hello Duniya" every 5 seconds.
+
+setInterval(() => {
+    console.log("Hello Duniya");
+}, 5000);
