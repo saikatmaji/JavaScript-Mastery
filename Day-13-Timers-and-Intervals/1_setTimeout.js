e6c19bae2c359bd92d9117d1d2dd43c082435073
saikatmaji <saikatmaji6295@gmail.com>
2026-08-15 22:59:20 +0530

    (feat) Day 13 Timers and Intervals

---
+++ Day-13-Timers-and-Intervals/1_setTimeout.js
@@ -0,0 +1,20 @@
+// setTimeout()
+
+// setTimeout() is used to execute a function only once after a specified amount of time.
+
+
+// For example, if we want to print "Hello World" after 5 seconds, we can use setTimeout().
+
+
+// Syntax
+
+// setTimeout(callbackFunction, delay);
+
+
+// Example
+
+// Print "Hello World" after 5 seconds.
+
+setTimeout(() => {
+    console.log("Hello World");
+}, 5000);
