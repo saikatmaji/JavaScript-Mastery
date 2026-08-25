0d919dc0d1cbde228737c0e41177cc7dda520e77
saikatmaji <saikatmaji6295@gmail.com>
2026-08-25 23:34:13 +0530

    (feat) Day 14 localStorage, sessionStorage and Cookies

--- Day-14-localStorage-sessionStorage-and-Cookies/2_sessionStorage.js
+++ Day-14-localStorage-sessionStorage-and-Cookies/2_sessionStorage.js
@@ -31,3 +31,25 @@ sessionStorage.setItem("name", "saikat");
 // setItem() can also be used to update an existing value. If the key already exists, its value will be replaced.
 
 sessionStorage.setItem("name", "raj");
+
+
+
+// 3. Fetch Data - getItem()
+
+// getItem() is used to retrieve data from sessionStorage.
+
+// Syntax:
+// sessionStorage.getItem(key);
+
+let val = sessionStorage.getItem("name");
+
+
+
+// 4. Remove Data - removeItem()
+
+// removeItem() is used to remove a specific item from sessionStorage.
+
+// Syntax:
+// sessionStorage.removeItem(key);
+
+sessionStorage.removeItem("name");
