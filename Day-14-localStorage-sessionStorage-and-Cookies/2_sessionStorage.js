dc7ca3b3b083310dc68445f5e4367465f6a158c0
saikatmaji <saikatmaji6295@gmail.com>
2026-08-25 23:32:23 +0530

    (feat) Day 14 localStorage, sessionStorage and Cookies

---
+++ Day-14-localStorage-sessionStorage-and-Cookies/2_sessionStorage.js
@@ -0,0 +1,33 @@
+// sessionStorage
+
+// sessionStorage is used to temporarily store data inside the browser. The stored data remains available while the current browser tab is open. Once the tab is closed, the stored data is removed.
+
+
+
+// Main sessionStorage Methods:-
+
+
+// There are 4 main operations-
+// 1. Store data => setItem()
+// 2. Update data => setItem()
+// 3. Fetch data => getItem()
+// 4. Remove data => removeItem()
+
+
+
+// 1. Store Data - setItem()
+
+// setItem() is used to store data in sessionStorage.
+
+// Syntax:
+// sessionStorage.setItem(key, value);
+
+sessionStorage.setItem("name", "saikat");
+
+
+
+// 2. Update Data - setItem()
+
+// setItem() can also be used to update an existing value. If the key already exists, its value will be replaced.
+
+sessionStorage.setItem("name", "raj");
