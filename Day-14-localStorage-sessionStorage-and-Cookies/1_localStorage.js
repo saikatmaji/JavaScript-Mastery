9350a55ed33bb6610141c5f90b6333aa6122cc17
saikatmaji <saikatmaji6295@gmail.com>
2026-08-25 23:24:19 +0530

    (feat) Day 14 localStorage, sessionStorage and Cookies

---
+++ Day-14-localStorage-sessionStorage-and-Cookies/1_localStorage.js
@@ -0,0 +1,32 @@
+// localStorage
+
+// localStorage is used to store data inside the browser. The stored data remains available even after closing or reopening the browser, until it is manually removed.
+
+
+
+// Main localStorage Methods:-
+
+// There are 4 main operations-
+// 1. Store data => setItem()
+// 2. Update data => setItem()
+// 3. Fetch data => getItem()
+// 4. Remove data => removeItem()
+
+
+
+// 1. Store Data - setItem()
+
+// setItem() is used to store data in localStorage.
+
+// Syntax:
+// localStorage.setItem(key, value);
+
+localStorage.setItem("name", "harsh");
+
+
+
+// 2. Update Data - setItem()
+
+// setItem() can also be used to update an existing value. If the key already exists, its value will be replaced.
+
+localStorage.setItem("name", "harshita");
