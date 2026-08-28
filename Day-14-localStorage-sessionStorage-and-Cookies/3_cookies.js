b517c651c703a9d8e4c00bf17b1798b02b8ff3b0
saikatmaji <saikatmaji6295@gmail.com>
2026-08-28 23:27:10 +0530

    (feat) Day 14 localStorage, sessionStorage and Cookies

---
+++ Day-14-localStorage-sessionStorage-and-Cookies/3_cookies.js
@@ -0,0 +1,23 @@
+// Cookies
+
+// Cookies are small pieces of data stored inside the browser. Cookies are mainly used to store information that can be sent between the browser and server.
+
+
+
+// Main Cookie Operations:-
+
+// There are mainly 3 basic operations-
+// 1. Store data   → document.cookie
+// 2. Fetch data   → document.cookie
+// 3. Remove data → document.cookie
+
+
+
+// 1. Store Data - Creating a Cookie
+
+// Cookies can be created using document.cookie.
+
+// Syntax:
+// document.cookie = "key=value";
+
+document.cookie = "name=saikat";
